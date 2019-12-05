8a7c1c4a95a38cc57cc2db0448bbaeafdbc5df3f
Teddy <brianrcdteodoro@gmail.com>
2019-12-05 14:14:00 +0800

    useEffect for functional components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import classes from './Cockpit.css';
 
 const cockpit = (props) => {
+	useEffect(
+		() => {
+			console.log('Cockpit.js - useEffect');
+			//HTTP request....
+			setTimeout(() => {
+				alert('Saving data to cloud'), 1000;
+			});
+		},
+		[ props.people ]
+	);
+
 	const styleClasses = [];
 	let btnClass = '';
 	if (props.showPeople) {
